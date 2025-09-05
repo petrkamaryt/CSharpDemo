0d8e603e86d51504b105c60e0db8b59c03d3b0cb
Kirill Cholodinskij <kiryl.khaladzdinski@thermofisher.com>
2025-09-05 13:20:40 +0200

    Create BMI Calculator

---
+++ CSharpDemo/BMICalculator/Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator
+{
+    public class Person
+    {
+        private double _weightKg;
+        private double _heightCm;
+
+        public double WeightKg
+        {
+            get { return _weightKg; }
+            set
+            {
+                if (value < 2 || value > 300)
+                {
+                    throw new ArgumentException("Weight must be between 2kg and 300kg.");
+                }
+                _weightKg = value;
+            }
+        }
+        
+        public double HeightCm
+        {
+            get { return _heightCm; }
+            set 
+            {
+                if (value < 30 || value > 250)
+                {
+                    throw new ArgumentException("Height must be between 30 and 250 cm.");
+                }
+                _heightCm = value;
+            }
+        }
+
+        public double BMI
+        {
+            get
+            {
+                double heightM = HeightCm / 100;
+                return WeightKg / (heightM * heightM);
+            }
+        }
+
+        public string GetCategory()
+        {
+            double bmi = BMI;
+            if (bmi < 18.5)
+                return "Underweight! Eat something!";
+            else if (bmi < 25)
+                return "Normal weight! Great job!";
+            else if (bmi < 30)
+                return "Overweight! Make some exercises!";
+            else
+                return "Obesity!";
+        }
+
+        public void PrintReport()
+        {
+            Console.WriteLine("\n---BMI Report---");
+            Console.WriteLine($"Weight: {WeightKg} kg");
+            Console.WriteLine($"Hight: {HeightCm} cm");
+            Console.WriteLine($"BMI {BMI:f2}");
+            Console.WriteLine($"Category: {GetCategory()}");
+        }
+    }
+}
---
+++ CSharpDemo/BMICalculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BMICalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the BMI Calculator!\n");
+
+            try
+            {
+                Person person = new Person();
+
+                Console.WriteLine("Enter your weight in kg: ");
+                person.WeightKg = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter your height in cm: ");
+                person.HeightCm = Convert.ToDouble(Console.ReadLine());
+
+                person.PrintReport();
+            }
+            catch (ArgumentException ex) 
+            {
+                Console.WriteLine($"Input error: {ex.Message}");
+            }
+            catch (FormatException) 
+            {
+                Console.WriteLine("Error: Please enter valid number.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An unexpected error occured: {ex.Message}");
+            }
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
